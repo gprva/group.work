d3223c791a09b4a27dd278e0a4ac6909d89f1c2a
gprva <148754074+gprva@users.noreply.github.com>
2024-02-22 20:20:48 +0600

    delete style for acardion

--- js/core.js
+++ js/core.js
@@ -61,11 +61,3 @@ const validateInputs = () => {
 
 
 //accardion
-const accardion = document.getElementsByClassName
-('contentBx');
-
-for (i = 0; i<accardion.length; i++){
-    accardion[i].addEventListener('click',function(){
-        this.classList.toggle('active')
-    })
-}
