6416386cd838e9fd96d16ea1174e3d87a04bb979
gprva <148754074+gprva@users.noreply.github.com>
2024-02-25 23:48:23 +0600

    add progress bar

--- js/core.js
+++ js/core.js
@@ -80,3 +80,16 @@ function boxHandler(e) {
     currentContent.style.maxHeight = 0; // скрываем контент
   }
 }
+
+//progress bar
+const numb = document.querySelector(".numb");
+let counter = 0;
+setInterval (() =>{
+    if(counter == 100){
+        clearInterval();
+    }else{
+        counter+=1;
+        numb.textContent = counter + "%"
+    }
+    
+},80)
